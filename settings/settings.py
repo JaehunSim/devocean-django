6a647e6148bdbfdac4b7ceb970936f47cdb45e67
jaehun.sim <jaehun.sim@sk.com>
2022-02-28 10:03:52 +0900

    DB Name 수정 (Heroku 배포용)

--- settings/settings.py
+++ settings/settings.py
@@ -47,7 +47,7 @@ WSGI_APPLICATION = 'settings.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
