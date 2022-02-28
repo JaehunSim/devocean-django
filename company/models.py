e5cd1c32643f17ee16c3c5fed4c7b9f5e91d9909
jaehun.sim <jaehun.sim@sk.com>
2022-02-28 10:39:43 +0900

    REST API Setup

--- company/models.py
+++ company/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=200)
+    field = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=500, blank=True)
+    latitude = models.FloatField(blank=True, null=True)
+    longitude = models.FloatField(blank=True, null=True)
+    total_employees = models.IntegerField(blank=True, null=True)
+    revenue = models.FloatField(blank=True, null=True)
+    operating_income = models.FloatField(blank=True, null=True)
+    net_income = models.FloatField(blank=True, null=True)
